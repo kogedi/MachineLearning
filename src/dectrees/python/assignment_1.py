914039c2ce428a3cf8165e90d4f8b5c2f69c1189
Konrad Dittrich <kogedi@gmx.de>
2023-09-18 20:55:57 +0200

    Boxplot and Lineplot A7

--- src/dectrees/python/assignment_1.py
+++ src/dectrees/python/assignment_1.py
@@ -2,6 +2,7 @@ import monkdata as m
 import dtree as d
 import PyQt5 as pq
 import drawtree_qt5 as drqt5
+import numpy as np
 
 # #Assignment 1
 # entropy = d.entropy(m.monk3)
@@ -25,7 +26,7 @@ import drawtree_qt5 as drqt5
     
     
 # #Assignment 5
-# t1 = d.buildTree(m.monk1, m.attributes, 10);
+#t1 = d.buildTree(m.monk1, m.attributes, 10);
 # t2 = d.buildTree(m.monk2, m.attributes);
 # t3 = d.buildTree(m.monk3, m.attributes);
 
@@ -39,6 +40,9 @@ import drawtree_qt5 as drqt5
 # print("Check Monk 3 Test",d.check(t3, m.monk3test))
 
 #Assignment 6
+#t1 = []
+#
+# t1.insert(1, A2(A5(+++-)A5(+A1(+A4(+-+)+)+-)A4(A5(--+A1(--+))--)))
 #drqt5.drawTree(t1)
 
 #Assignment 7
@@ -58,68 +62,96 @@ def partition(data, fraction):
     return ldata[:breakPoint], ldata[breakPoint:]
 
 fraction = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-#fraction = [0.6, 0.7]
-accuracyfraction = []
-
-for fracind in range(len(fraction)):
-    print("For fraction of ", fraction[fracind])
-    
-    monk3train, monk3val = partition(m.monk3, fraction[fracind])
-    # Built up the tree of Monk 3 used since it's the simplest one.
-    t3 = d.buildTree(monk3train, m.attributes)
-    PrunChances = 0
-    PrunChances= d.allPruned(t3)
-    j = 0
-    maxaccuracy = []
-    repeat = True
-    while repeat == True:
-        accuracy = []
+#fraction = [0.8, 0.8]
+entireRuns = 50
+N = 0
+accuracyfraction = np.zeros((len(fraction), int(entireRuns)))
+for N in range(entireRuns):
+    for fracind in range(len(fraction)):
+        print("For fraction of ", fraction[fracind])
         
-        for k in range(len(PrunChances)):
-            #print("Check",d.check(PrunChances[k], monk3val))
-            accuracy.insert(k, round(d.check(PrunChances[k], monk3val), 5))
-            #print(k,":",accuracy[k], "  for tree:", PrunChances[k])
-        
-        'Determin the best subset in terms of accuracy'
-        maxaccuracy.insert(j, max(accuracy)) 
-        #print("maxaccuracy[j]", maxaccuracy[j]) 
-        maxaccurayID = accuracy.index(maxaccuracy[j])
-        t3sub = PrunChances[maxaccurayID]
-        print("Best subset is ",maxaccurayID,"with an accuracy of",maxaccuracy[j])
-        print(t3sub)
-        print(" ")
-        # Prune the subset again
+        monk1train, monk1val = partition(m.monk1, fraction[fracind])
+        monk3train, monk3val = partition(m.monk3, fraction[fracind])
+        # Built up the tree of Monk 3 used since it's the simplest one.
+        t3 = d.buildTree(monk3train, m.attributes)
+        t1 = d.buildTree(monk1train, m.attributes)
         PrunChances = 0
-        PrunChances = d.allPruned(t3sub)
-        if len(maxaccuracy) > 1:
-            #print("len", len(maxaccuracy))
-            if maxaccuracy[j] <= maxaccuracy[j-1]:
-                repeat = False
-                print("Rejected. Best Dataset beforecls")
+        PrunChances= d.allPruned(t3)
+        j = 0
+        maxaccuracy = []
+        repeat = True
+        while repeat == True:
+            accuracy = []
             
+            for k in range(len(PrunChances)):
+                #print("Check",d.check(PrunChances[k], monk3val))
+                accuracy.insert(k, round(d.check(PrunChances[k], monk3val), 5))
+                #print(k,":",accuracy[k], "  for tree:", PrunChances[k])
+            
+                'Determin the best subset in terms of accuracy'
+                if accuracy[k] == 0:
+                    accuracy.insert(k, 0.0000001)
+            maxaccuracy.insert(j, max(accuracy)) 
+            #print("maxaccuracy[j]", maxaccuracy[j]) 
+            maxaccurayID = accuracy.index(maxaccuracy[j])
+            t3sub = PrunChances[maxaccurayID]
+            print("Best subset is ",maxaccurayID,"with an accuracy of",maxaccuracy[j])
+            print(t3sub)
+            #drqt5.drawTree(t3sub)
+            print(" ")
+            # Prune the subset again
+            PrunChances = 0
+            PrunChances = d.allPruned(t3sub)
+            if len(PrunChances) < 2:
+                break
                 
-        
-        j += 1
-    accuracyfraction.insert(fracind, maxaccuracy)
+            if len(maxaccuracy) > 1:
+                #print("len", len(maxaccuracy))
+                if maxaccuracy[j] <= maxaccuracy[j-1]:
+                    repeat = False
+                    print("Rejected. Best Dataset beforecls")
+            
+            accuracyfraction[fracind,N] = maxaccuracy[j]
+            j += 1
+    #maxaccuracyfraction = max(accuracyfraction)
+    #maxaccuracyfractionID = accuracyfraction.index(maxaccuracyfraction)
+    #print("The highest accuracy is generated with a fraction of",fraction[maxaccuracyfractionID], " w/ value",maxaccuracyfraction )
     
- 
-maxaccuracyfraction = max(accuracyfraction)
-maxaccuracyfractionID = accuracyfraction.index(maxaccuracyfraction)
-print("The highest accuracy is generated with a fraction of",fraction[maxaccuracyfractionID], " w/ value",maxaccuracyfraction )
+#Datenaufbereitung
+print("accuracyfraction", accuracyfraction)
+#Statistics
+#mean = 
 
-    
-    # Redo for a new dataset
-    # PrunChances = 0
-    # PrunChances = d.allPruned(t3sub)
-    
-    # for i in range(len(PrunChances)):
-    #     accuracy.insert(i, round(d.check(PrunChances[i], monk3val), 5))
-    #     print(i,":",accuracy[i], "  for tree:", PrunChances[i])
-        
-    # maxaccuracy = max(accuracy)
-    # accuracyfraction.insert(fracind, maxaccuracy)
-    # maxaccurayID = accuracy.index(maxaccuracy)
-    
-    # t3subsub = PrunChances[maxaccurayID]
-    # print("Best SubSubset is ",maxaccurayID,"weith an accuracy of",maxaccuracy, t3subsub)
-    # print("")
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Erstelle sechs Boxplots
+plt.figure(figsize=(10, 6))
+x_values = []
+for i, data in enumerate(accuracyfraction):
+    plt.subplot(2, 3, i + 1)  # Subplots für jeden Boxplot
+    mean = np.mean(data)
+    x_values.insert(i, mean)
+    std_dev = np.std(data)
+    sns.boxplot(data=data,width=0.5)
+    plt.title(f'Fraction: {fraction[i]}\nMean = {mean:.2f}, StdDev = {std_dev:.2f}')
+    plt.xlabel('')
+    plt.ylabel('Accuracy')
+
+plt.tight_layout()  # Damit die Plots nicht überlappen
+plt.show()
+
+# Beispiel-Array mit 6 Werten
+data = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+
+# X-Werte als Fraktionen von 0 bis 1 erstellen 
+
+
+# Plot erstellen
+plt.plot(data, x_values, marker='o', linestyle='-')
+plt.xlabel('Fraction')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs. Fraction')
+plt.grid(True)  # Raster anzeigen, um die Werte besser abzulesen
+plt.show()
