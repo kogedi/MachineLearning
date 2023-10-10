8e655b7f81f7dfae7c7120944c199c4fbadab390
Konrad Dittrich <kogedi@gmx.de>
2023-10-10 18:06:30 +0200

    SVM2 indicator function to be implemented

--- src/svm/SVM2.py
+++ src/svm/SVM2.py
@@ -3,6 +3,7 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from scipy.optimize import Bounds
 
+numpy.random.seed(100)
 
 classA = numpy. concatenate (
     (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
@@ -22,9 +23,27 @@ targets = targets [ permute ]
 
 
 def kernel (arrayx, arrayy):
+    """linear Kernel
+
+    Args:
+        arrayx (_type_): _description_
+        arrayy (_type_): _description_
+
+    Returns:
+        scalar: _description_
+    """
     return numpy.dot(arrayx, arrayy)
 
-def compute_kernel_matrix(data, kernel_function):
+def pre_calculate_P_kernel(data, kernel_function):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+        kernel_function (_type_): _description_
+
+    Returns:
+        _type_: prestep for Kernelmatrix called "Pij"
+    """
 
     kernel_matrix = numpy.zeros((N, N))
 
@@ -32,26 +51,27 @@ def compute_kernel_matrix(data, kernel_function):
         for j in range(N):
             kernel_matrix[i, j] = targets[i]*targets[j]*kernel_function(data[i], data[j])
 
-    return kernel_matrix #pij
+    return kernel_matrix 
 
 # Berechnung der Kernelmatrix schon mit ti und tj 
-kernel_matrix = compute_kernel_matrix(inputs, kernel)
+#P_matrix = pre_calculate_P_kernel(inputs, kernel)
+kernel_matrix = pre_calculate_P_kernel(inputs, kernel)
 
 # Ausgabe der Kernelmatrix
 #print("Kernel_matrix",kernel_matrix)
 
-# IMPLEMENTATION
+#************ IMPLEMENTATION ******************
 #
 # 0 - setup variables
 # 1 - objective
-# 2 - start
+# 2 - start x0
 # 3 - bounds
 # 4 - constraints
-# 5 - MINIMIZE (1, 2, 3, 4)
+# 5 - MINIMIZE function (1, 2, 3, 4)
 # 6 - resulting vector
 
 # 0 - setup variables
-alpha = [0.0] * N
+alpha = numpy.zeros(N) #[0.0] * N
 
 # 1 - objective
 def objective (alpha):
@@ -72,22 +92,22 @@ start = numpy.zeros(N) #Why this start vector step-up. Maybe better np.zeros(N)
 #print("Start",start)
 
 # 3 - bounds
-C = 1.0  #Slack variable C
-B = [(0, C) for b in range(N)]
+c = 1.0  #Slack variable C
+B = [(0, c) for b in range(N)]
 
 # 4 - constraints
 #gleichung = 0 #TODO Wofür?
-def zerofun(alpha, targets):
-    # gleichung = 0.0
-    # for i in range(N):
-    #     gleichung =  alpha[i] * targets[i]
-    gleichung = numpy.dot(alpha,targets)
-    return gleichung 
+# def zerofun(alpha, targets):
+#     # gleichung = 0.0
+#     # for i in range(N):
+#     #     gleichung =  alpha[i] * targets[i]
+#     gleichung = numpy.dot(alpha,targets)
+#     return gleichung 
 
 zerofun = lambda alpha: numpy.dot(alpha,targets)
     
 #constraints
-XC = {'type':'eq', 'fun':zerofun},
+XC = {'type':'eq', 'fun':zerofun} #contraints zerofun to be ZERO
 
 # 5 - MINIMIZE (1, 2, 3, 4)
 ret = minimize(objective, start, bounds=B, constraints = XC)
@@ -98,6 +118,7 @@ alphamin = ret['x']
 print("alphamin")
 print(alphamin)
 plt.plot(alphamin)
+plt.show()
 
 
 threshold = 1e-5  
@@ -105,24 +126,56 @@ non_zero_alphas = []
 nz_x= []
 nz_t= []
 
-# TODO What is the part below doing?
+# Separate non-zero alpha-Values
+
 for i in range(len(alphamin)):
     if alphamin[i] > threshold: 
         non_zero_alphas.append(alphamin[i])
         nz_x.append(inputs[i])
         nz_t.append(targets[i])
+        
+      
+
+def calculate_kernel_vector(s,inputs,kernel_function):
+    kernel_vektor = []  
+    for i in range(N):
+            kernel_vektor.append(kernel_function(s, inputs[i]))
+    return kernel_vektor          
     
-def calculate_b(alpha, targets, kernel_matrix, s):
+def calculate_b(alpha, targets, inputs, s,kernel_function):
+    """calculates the threshhold value b
+
+    Args:
+        alpha (vector): optimized values of alpha (decision variable)
+        targets (vector): target values for data points
+        kernel_matrix (matrix): already calculates Kernel matrix to safe computation
+        s (vector): new data vector to be classified
+
+    Returns:
+        scalar: offset of the hyperplane (here: 2-d case)
+    """
+    
     b_sum = 0.0
     for i in range(len(alpha)):
-        b_sum += alpha[i] * targets[i] * kernel_matrix[s, i]
+        b_sum += alpha[i] * targets[i] * calculate_kernel_vector(s,inputs,kernel_function)[i]
     b = b_sum - targets[s]
     return b
 
-def indicator(alpha, targets, kernel_matrix, s):
+def indicator(alpha, targets, kernel_matrix, s,kernel_function):
+    """classification of a new data point s
+
+    Args:
+        alpha (vector): optimal decision variables
+        targets (vector): labels of the training data
+        kernel_matrix (matrix): precalculated Kernel matrix
+        s (_type_): new datapoint
+
+    Returns:
+        _type_: _description_
+    """
     indicator_s = 0.0
     for i in range(N):
-        indicator_s += alpha[i] * targets[i] * kernel_matrix[s, i]
+        indicator_s += alpha[i] * targets[i] * calculate_kernel_vector(s,inputs,kernel_function)[i]
     indi = indicator_s - calculate_b (alpha, targets, kernel_matrix, s)
     return indi
 
