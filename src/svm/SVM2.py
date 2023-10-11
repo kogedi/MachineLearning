6dec16aa64c10e2692d2956e5067d34f7d269a1a
Konrad Dittrich <kogedi@gmx.de>
2023-10-11 10:31:43 +0200

    New util file for better orga. 1stclassifi results

--- src/svm/SVM2.py
+++ src/svm/SVM2.py
@@ -2,14 +2,18 @@ import numpy, random, math
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from scipy.optimize import Bounds
+from util import *
 
+#Set Random to same values for every iteration
 numpy.random.seed(100)
 
+# Data Generation
 classA = numpy. concatenate (
     (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
      numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
-classB = numpy.random.randn(20, 2) * 0.2 + [0.0 , -0.5]
 
+#print("classA",classA)
+classB = numpy.random.randn(20, 2) * 0.2 + [0.0 , -0.5]
 
 inputs = numpy. concatenate (( classA , classB ))
 targets = numpy. concatenate ((numpy.ones(classA.shape[0]) , - numpy.ones ( classB.shape[ 0 ] ) ) ) 
@@ -22,40 +26,9 @@ inputs = inputs [ permute , : ]
 targets = targets [ permute ]
 
 
-def kernel (arrayx, arrayy):
-    """linear Kernel
-
-    Args:
-        arrayx (_type_): _description_
-        arrayy (_type_): _description_
-
-    Returns:
-        scalar: _description_
-    """
-    return numpy.dot(arrayx, arrayy)
-
-def pre_calculate_P_kernel(data, kernel_function):
-    """_summary_
-
-    Args:
-        data (_type_): _description_
-        kernel_function (_type_): _description_
-
-    Returns:
-        _type_: prestep for Kernelmatrix called "Pij"
-    """
-
-    kernel_matrix = numpy.zeros((N, N))
-
-    for i in range(N):
-        for j in range(N):
-            kernel_matrix[i, j] = targets[i]*targets[j]*kernel_function(data[i], data[j])
-
-    return kernel_matrix 
-
 # Berechnung der Kernelmatrix schon mit ti und tj 
 #P_matrix = pre_calculate_P_kernel(inputs, kernel)
-kernel_matrix = pre_calculate_P_kernel(inputs, kernel)
+kernel_matrix = calculate_P_kernel_matrix(inputs,targets)
 
 # Ausgabe der Kernelmatrix
 #print("Kernel_matrix",kernel_matrix)
@@ -69,6 +42,9 @@ kernel_matrix = pre_calculate_P_kernel(inputs, kernel)
 # 4 - constraints
 # 5 - MINIMIZE function (1, 2, 3, 4)
 # 6 - resulting vector
+# 7 - plot alphamin
+# 8 - Separate non-zero alpha-Values and plot
+# 9 - Plotting decision boundaries
 
 # 0 - setup variables
 alpha = numpy.zeros(N) #[0.0] * N
@@ -115,73 +91,27 @@ ret = minimize(objective, start, bounds=B, constraints = XC)
 # 6 - resulting vector
 alphamin = ret['x']
 
-print("alphamin")
-print(alphamin)
+# 7 - plot alphamin
+#print("alphamin")
+#print(alphamin)
 plt.plot(alphamin)
 plt.show()
 
-
+# 8 - Separate non-zero alpha-Values and plot
 threshold = 1e-5  
 non_zero_alphas = []
 nz_x= []
 nz_t= []
-
-# Separate non-zero alpha-Values
 
 for i in range(len(alphamin)):
     if alphamin[i] > threshold: 
         non_zero_alphas.append(alphamin[i])
         nz_x.append(inputs[i])
         nz_t.append(targets[i])
+plt.plot(non_zero_alphas)
+plt.show()
         
-      
-
-def calculate_kernel_vector(s,inputs,kernel_function):
-    kernel_vektor = []  
-    for i in range(N):
-            kernel_vektor.append(kernel_function(s, inputs[i]))
-    return kernel_vektor          
-    
-def calculate_b(alpha, targets, inputs, s,kernel_function):
-    """calculates the threshhold value b
-
-    Args:
-        alpha (vector): optimized values of alpha (decision variable)
-        targets (vector): target values for data points
-        kernel_matrix (matrix): already calculates Kernel matrix to safe computation
-        s (vector): new data vector to be classified
-
-    Returns:
-        scalar: offset of the hyperplane (here: 2-d case)
-    """
-    
-    b_sum = 0.0
-    for i in range(len(alpha)):
-        b_sum += alpha[i] * targets[i] * calculate_kernel_vector(s,inputs,kernel_function)[i]
-    b = b_sum - targets[s]
-    return b
-
-def indicator(alpha, targets, kernel_matrix, s,kernel_function):
-    """classification of a new data point s
-
-    Args:
-        alpha (vector): optimal decision variables
-        targets (vector): labels of the training data
-        kernel_matrix (matrix): precalculated Kernel matrix
-        s (_type_): new datapoint
-
-    Returns:
-        _type_: _description_
-    """
-    indicator_s = 0.0
-    for i in range(N):
-        indicator_s += alpha[i] * targets[i] * calculate_kernel_vector(s,inputs,kernel_function)[i]
-    indi = indicator_s - calculate_b (alpha, targets, kernel_matrix, s)
-    return indi
-
-# 6 Plotting
-
-#See the data
+# 9 - Plotting decision boundaries
 plt.plot([p[0] for p in classA] ,
          [p[1] for p in classA] ,
          'b.') 
@@ -194,15 +124,19 @@ plt.savefig('svmplot.pdf') # Save a copy in a file
 plt .show() # Show the plot on the screen
 print('test')
 
-#6.1 Plotting the Decision Boundary
-# xgrid=numpy. linspace (-5, 5)
-# ygrid=numpy. linspace (-4, 4)
 
-# grid=numpy. array ( [ [ indicator (x , y)
-#                        for x in xgrid ]
-#                      for y in ygrid ])
+# 10 - Plotting the Decision Boundary
+xgrid=numpy. linspace (-5, 5)
+ygrid=numpy. linspace (-4, 4)
 
-# plt . contour (xgrid , ygrid , grid ,
-#                (-1.0, 0.0 , 1.0),
-#                colors=('red', 'black' , 'blue' ),
-#                linewidths =(1, 3 , 1))
+grid=numpy. array ( [ [ indicator(non_zero_alphas, nz_t, nz_x, (x , y))
+                       for x in xgrid ]
+                     for y in ygrid ])
+
+grid = numpy.array(grid)
+
+plt . contour (xgrid , ygrid , grid ,
+               (-1.0, 0.0 , 1.0),
+               colors=('red', 'black' , 'blue' ),
+               linewidths =(1, 3 , 1))
+plt.show()
---
+++ src/svm/util.py
@@ -0,0 +1,82 @@
+import numpy, random, math 
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from scipy.optimize import Bounds
+
+def kernel (arrayx, arrayy):
+    """linear Kernel
+
+    Args:
+        arrayx (_type_): _description_
+        arrayy (_type_): _description_
+
+    Returns:
+        scalar: _description_
+    """
+    return numpy.dot(arrayx, arrayy)
+
+def calculate_P_kernel_matrix(data,targets):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+
+    Returns:
+        _type_: prestep for Kernelmatrix called "Pij"
+    """
+    N = data.shape [0]
+    P_kernel_matrix = numpy.zeros((N, N))
+
+    for i in range(N):
+        for j in range(N):
+            P_kernel_matrix[i, j] = targets[i]*targets[j]*kernel(data[i], data[j])
+
+    return P_kernel_matrix
+
+def calculate_kernel_vector(s, data):
+    N = len(data)
+    kernel_vektor = []  
+    for i in range(N):
+            kernel_vektor.append(kernel(s, data[i]))
+    return kernel_vektor 
+
+
+def calculate_b(non_zero_alpha, non_zero_targets, non_zero_data, s):
+    """calculates the scalar threshold value b
+
+    Args:
+        alpha (vector): optimized values of alpha (decision variable)
+        targets (vector): target values for data points
+        kernel_matrix (matrix): already calculates Kernel matrix to safe computation
+        s (vector): new data vector to be classified
+
+    Returns:
+        b (scalar): threshold value b
+    """
+    #Choose one support vector out of the list
+    sv = 1
+    
+    kernel_vector = calculate_kernel_vector(s, non_zero_data)
+    b_sum = 0.0
+    for i in range(len(non_zero_alpha)):
+        b_sum += non_zero_alpha[i] * non_zero_targets[i] *kernel_vector[i]
+    b = b_sum - non_zero_targets[sv]
+    return b
+
+def indicator(alpha, targets, data, s):
+    """classification of a new data point s
+
+    Args:
+        alpha (vector): optimal decision variables
+        targets (vector): labels of the training data
+        data (matrix): precalculated Kernel matrix
+        s (vectors): new datapoint
+
+    Returns:
+        scalar: indicates the class [-1, 0, +1]
+    """
+    indicator_s = 0.0
+    for i in range(len(alpha)):
+        indicator_s += alpha[i] * targets[i] * calculate_kernel_vector(s, data)[i]
+    indi = indicator_s - calculate_b (alpha, targets, data, s)
+    return indi
